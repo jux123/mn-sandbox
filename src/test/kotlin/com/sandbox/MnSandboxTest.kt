c8683437a4f3501556a021158acafd386cb9bc31
Juhan Voolaid <juhan.voolaid@gmail.com>
2025-06-07 12:41:49 +0300

    Enhance MnSandboxTest to include HTTP client and add /hello endpoint test

--- src/test/kotlin/com/sandbox/MnSandboxTest.kt
+++ src/test/kotlin/com/sandbox/MnSandboxTest.kt
@@ -2,11 +2,22 @@ package com.sandbox
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
 
 @MicronautTest
-class MnSandboxTest(private val application: EmbeddedApplication<*>): StringSpec({
+class MnSandboxTest(
+    private val application: EmbeddedApplication<*>,
+    @Client("/") private val httpClient: HttpClient
+    ): StringSpec({
 
     "test the server is running" {
         assert(application.isRunning)
+    }
+
+    "test /hello endpoint" {
+        val response = httpClient.toBlocking().retrieve("/hello")
+        response shouldBe "Hello world"
     }
 })
