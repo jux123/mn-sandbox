29a14d8c6e4c2a1747c281daaac009b692667e37
Juhan Voolaid <juhan.voolaid@gmail.com>
2025-06-08 15:58:33 +0300

    added logger

--- src/main/kotlin/com/sandbox/controller/HelloWorldController.kt
+++ src/main/kotlin/com/sandbox/controller/HelloWorldController.kt
@@ -3,12 +3,17 @@ package com.sandbox.controller
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @Controller
 class HelloWorldController {
 
+    private val log: Logger = LoggerFactory.getLogger(HelloWorldController::class.java)
+
     @Get("/hello", produces = [MediaType.TEXT_PLAIN])
     fun hello(): String {
+        log.info("Hello, World!")
         return "Hello world"
     }
 }
